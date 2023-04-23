609aad3716ba0729514c4d02e890a328e3b44c83
Matheus Buschermoehle <matheusbuschermoehle@gmail.com>
2023-04-23 12:34:46 -0300

    Project created

---
+++ spring-spotmusic/src/main/java/com/matheusbus/springspotmusic/SpringSpotmusicApplication.java
@@ -0,0 +1,13 @@
+package com.matheusbus.springspotmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSpotmusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSpotmusicApplication.class, args);
+	}
+
+}
